784321860e93d4cff0cd7216a8d869f78053905d
Vladimir Saakyan <polossless@gmail.com>
2019-03-17 22:19:10 +0200

    Updates

---
+++ precache-manifest.07d0666d745b5f9a589a6ce9ec8d6f69.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = [
+  {
+    "revision": "44c0272975f2c68082b8ab833e8eed77",
+    "url": "/atomspace/atomspace.od.ua/static/media/abc-edu.44c02729.svg"
+  },
+  {
+    "revision": "6af78c22dd8c0c7a675a",
+    "url": "/atomspace/atomspace.od.ua/static/css/main.cc16aa36.chunk.css"
+  },
+  {
+    "revision": "81adc925e3332340cda5e406c5a66e18",
+    "url": "/atomspace/atomspace.od.ua/static/media/background-contacts.81adc925.png"
+  },
+  {
+    "revision": "a1fae60da70690988d8c",
+    "url": "/atomspace/atomspace.od.ua/static/js/1.a1fae60d.chunk.js"
+  },
+  {
+    "revision": "7be379f2b5917bac1f48",
+    "url": "/atomspace/atomspace.od.ua/static/js/runtime~main.7be379f2.js"
+  },
+  {
+    "revision": "490f8227429536b0d30383b118733d5c",
+    "url": "/atomspace/atomspace.od.ua/static/media/i-need-more-space-2-shirt.490f8227.png"
+  },
+  {
+    "revision": "0255d6e0a485703966a8f21b725b80c2",
+    "url": "/atomspace/atomspace.od.ua/static/media/nlo-shirt.0255d6e0.png"
+  },
+  {
+    "revision": "bdf421b9454896992957d81da5a7b639",
+    "url": "/atomspace/atomspace.od.ua/static/media/about_photo_1.bdf421b9.png"
+  },
+  {
+    "revision": "adf6365a8f4ede04c2736820ba744e55",
+    "url": "/atomspace/atomspace.od.ua/static/media/about_photo_2.adf6365a.png"
+  },
+  {
+    "revision": "616a213618733e9be05e996899da498f",
+    "url": "/atomspace/atomspace.od.ua/static/media/about_photo_3.616a2136.png"
+  },
+  {
+    "revision": "04edd3b377086c62d8a1c1786910bf36",
+    "url": "/atomspace/atomspace.od.ua/static/media/about_photo_4.04edd3b3.png"
+  },
+  {
+    "revision": "81b382cea2c5a4df03a6d1a5e7fcf3ae",
+    "url": "/atomspace/atomspace.od.ua/static/media/rocket-donate.81b382ce.png"
+  },
+  {
+    "revision": "e1afebe6fafbd513071dcf0f5a187358",
+    "url": "/atomspace/atomspace.od.ua/static/media/AtomSpace-logo-vertical.e1afebe6.png"
+  },
+  {
+    "revision": "d224071b91e68ba4194f7a6a65fc7d78",
+    "url": "/atomspace/atomspace.od.ua/static/media/i-need-more-space-shirt.d224071b.png"
+  },
+  {
+    "revision": "9116865c4cc8f396b7cbca637eddee71",
+    "url": "/atomspace/atomspace.od.ua/static/media/mind-blog.9116865c.png"
+  },
+  {
+    "revision": "078e0888cca0401e6413998a93a54f32",
+    "url": "/atomspace/atomspace.od.ua/static/media/people-blog.078e0888.png"
+  },
+  {
+    "revision": "25a510aab0253a3484d302ae412a98e0",
+    "url": "/atomspace/atomspace.od.ua/static/media/puzzle-blog.25a510aa.png"
+  },
+  {
+    "revision": "d55b2c75ba2f22f7f8d35cce84cb47a7",
+    "url": "/atomspace/atomspace.od.ua/static/media/background-family-bot.d55b2c75.png"
+  },
+  {
+    "revision": "2e2ff219f9b4917b511f4db21a29b4e6",
+    "url": "/atomspace/atomspace.od.ua/static/media/facebook-light.2e2ff219.svg"
+  },
+  {
+    "revision": "86729c55e07721bd7dc6c4abc7c72020",
+    "url": "/atomspace/atomspace.od.ua/static/media/instagram-light.86729c55.svg"
+  },
+  {
+    "revision": "93e19a6bfe2d145754660a4eeae3dace",
+    "url": "/atomspace/atomspace.od.ua/static/media/linkedin-light.93e19a6b.svg"
+  },
+  {
+    "revision": "0d049bc90d6537a294cba69a28e8c13a",
+    "url": "/atomspace/atomspace.od.ua/static/media/circle.0d049bc9.svg"
+  },
+  {
+    "revision": "1110d918baa16c8bd1052dbd1fe6e794",
+    "url": "/atomspace/atomspace.od.ua/static/media/instagram-dark.1110d918.svg"
+  },
+  {
+    "revision": "789c4ee4b90477c441145d7297ba0c11",
+    "url": "/atomspace/atomspace.od.ua/static/media/facebook-dark.789c4ee4.svg"
+  },
+  {
+    "revision": "f28369429d45125c2f501be54383f37e",
+    "url": "/atomspace/atomspace.od.ua/static/media/circle-dark.f2836942.svg"
+  },
+  {
+    "revision": "25ceca6606e0c1648d58db46b1170399",
+    "url": "/atomspace/atomspace.od.ua/static/media/rocket-edu.25ceca66.svg"
+  },
+  {
+    "revision": "b425a23a307479a4cd92314870ff88f3",
+    "url": "/atomspace/atomspace.od.ua/static/media/tasks-edu.b425a23a.svg"
+  },
+  {
+    "revision": "c2e9d46e7660ffa926f35bd64068e2ca",
+    "url": "/atomspace/atomspace.od.ua/static/media/css-html-edu.c2e9d46e.svg"
+  },
+  {
+    "revision": "ed0f5a34e695254d1f66080916fddf3c",
+    "url": "/atomspace/atomspace.od.ua/static/media/rocket-from-monitor-edu.ed0f5a34.svg"
+  },
+  {
+    "revision": "38d816b6cf4cc00f3c77bcb2ee6fa83f",
+    "url": "/atomspace/atomspace.od.ua/static/media/js-edu.38d816b6.svg"
+  },
+  {
+    "revision": "6af78c22dd8c0c7a675a",
+    "url": "/atomspace/atomspace.od.ua/static/js/main.6af78c22.chunk.js"
+  },
+  {
+    "revision": "37eac0e61863d3d76dc98c8cc6420ef9",
+    "url": "/atomspace/atomspace.od.ua/static/media/python-edu.37eac0e6.svg"
+  },
+  {
+    "revision": "6013c11fea98169fd0be8e2a0f216505",
+    "url": "/atomspace/atomspace.od.ua/static/media/close.6013c11f.svg"
+  },
+  {
+    "revision": "4c9a056834beb59ad1be0beb312bedfb",
+    "url": "/atomspace/atomspace.od.ua/static/media/background-blog.4c9a0568.png"
+  },
+  {
+    "revision": "3d4eb89dcf389d3222513fd736547a8c",
+    "url": "/atomspace/atomspace.od.ua/static/media/about_atom-space.3d4eb89d.svg"
+  },
+  {
+    "revision": "434ed8a1e55f80c7399e510f8b01555f",
+    "url": "/atomspace/atomspace.od.ua/static/media/wordpress-edu.434ed8a1.svg"
+  },
+  {
+    "revision": "9951caa4eafb756e1a4172cf4421108d",
+    "url": "/atomspace/atomspace.od.ua/static/media/background-store.9951caa4.png"
+  },
+  {
+    "revision": "9d0f003e1ac86b186cb8d1ab9869bd5e",
+    "url": "/atomspace/atomspace.od.ua/static/media/background-form.9d0f003e.png"
+  },
+  {
+    "revision": "a3a494acecc840e8e4fe82d76d414d41",
+    "url": "/atomspace/atomspace.od.ua/static/media/background-edu.a3a494ac.png"
+  },
+  {
+    "revision": "97472e6dbab21d2dc91e9dee0d11b2f1",
+    "url": "/atomspace/atomspace.od.ua/static/media/background-about.97472e6d.png"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/atomspace/atomspace.od.ua/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/atomspace/atomspace.od.ua/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/atomspace/atomspace.od.ua/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/atomspace/atomspace.od.ua/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "0e0723896325c13567bc0c1032f99b18",
+    "url": "/atomspace/atomspace.od.ua/static/media/background-family.0e072389.png"
+  },
+  {
+    "revision": "bf243bcbf81560535385c47725f57dfe",
+    "url": "/atomspace/atomspace.od.ua/static/media/OpenSans-Italic.bf243bcb.ttf"
+  },
+  {
+    "revision": "fc17ea62c914b2aaa9e89ecc519a8cb3",
+    "url": "/atomspace/atomspace.od.ua/static/media/OpenSans-SemiBoldItalic.fc17ea62.ttf"
+  },
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "/atomspace/atomspace.od.ua/static/media/OpenSans-Regular.d7d5d458.ttf"
+  },
+  {
+    "revision": "f40598dd8ea8593e91ab28aa48130cf1",
+    "url": "/atomspace/atomspace.od.ua/static/media/OpenSans-BoldItalic.f40598dd.ttf"
+  },
+  {
+    "revision": "617beb2703a6fccd227ab45754d73440",
+    "url": "/atomspace/atomspace.od.ua/static/media/OpenSans-ExtraBoldItalic.617beb27.ttf"
+  },
+  {
+    "revision": "54b4443404115cef4af765596ccf0ed3",
+    "url": "/atomspace/atomspace.od.ua/static/media/OpenSans-LightItalic.54b44434.ttf"
+  },
+  {
+    "revision": "e1c83f9474e0cc1d84a13c6d1ddf3ca5",
+    "url": "/atomspace/atomspace.od.ua/static/media/OpenSans-SemiBold.e1c83f94.ttf"
+  },
+  {
+    "revision": "f5331cb6372b6c0d8baf2dd7e200498c",
+    "url": "/atomspace/atomspace.od.ua/static/media/OpenSans-Bold.f5331cb6.ttf"
+  },
+  {
+    "revision": "49f89e34d03233b1f27788f75df7a40a",
+    "url": "/atomspace/atomspace.od.ua/static/media/OpenSans-ExtraBold.49f89e34.ttf"
+  },
+  {
+    "revision": "9ff12f694e5951a6f51a9d63b05062e7",
+    "url": "/atomspace/atomspace.od.ua/static/media/OpenSans-Light.9ff12f69.ttf"
+  },
+  {
+    "revision": "4c15d2f0e04c39cca69b5d8152fdbf40",
+    "url": "/atomspace/atomspace.od.ua/static/media/anna-derevyanko.4c15d2f0.png"
+  },
+  {
+    "revision": "1ab75fbd657e6d5d50f261d814036234",
+    "url": "/atomspace/atomspace.od.ua/static/media/kseniya-yarmolovich.1ab75fbd.png"
+  },
+  {
+    "revision": "5bfe9d6ca1c0bc43f050fb27836d77be",
+    "url": "/atomspace/atomspace.od.ua/static/media/background-main.5bfe9d6c.png"
+  },
+  {
+    "revision": "96f7b1b7e9d417e839fd0ddb376fa92c",
+    "url": "/atomspace/atomspace.od.ua/static/media/veronika-ilnitckaya.96f7b1b7.png"
+  },
+  {
+    "revision": "a1fae60da70690988d8c",
+    "url": "/atomspace/atomspace.od.ua/static/css/1.547c425e.chunk.css"
+  },
+  {
+    "revision": "15c92072a90220ea788836e75e5904a7",
+    "url": "/atomspace/atomspace.od.ua/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/atomspace/atomspace.od.ua/precache-manifest.07d0666d745b5f9a589a6ce9ec8d6f69.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/atomspace/atomspace.od.ua/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
